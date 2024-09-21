6fff6927f82b550fb01d41dbf09c0dfa4c55cebd
SalvatoreC94 <salvatorecozzolino.1594@gmail.com>
2024-09-21 16:58:36 +0200

    bozza header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,50 +1,87 @@
+<template>
+  <header>
+    <!-- Header Superiore -->
+    <div class="header-superiore ">
+      <div>
+        (001) 88451234 88455438
+      </div>
+      <nav>
+        <ul class="">
+          <li class="me-3">
+            <a href="https://facebook.com" target="_blank">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+          </li>
+          <li class="me-3">
+            <a href="https://instagram.com" target="_blank">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://pinterest.com" target="_blank">
+              <i class="fab fa-pinterest-p"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+    <!-- Header Inferiore -->
+    <div class="header-inferiore">
+      <div class="logo">
+        <img src="/img/images/cropped-Group-39-2x.png" alt="Logo 1" />
+      </div>
+      <nav class="navbar">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Service</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Showcase</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="purchase">
+        <input type="button">Purchase
+      </div>
+    </div>
+  </header>
+</template>
+
 <script>
 export default {
   name: 'AppHeader',
 };
 </script>
 
-<template>
-  <header>
-    <!-- Header Superiore -->
-    <div class="header-superiore d-flex justify-content-between">
-      <div>Elemento Sinistra</div>
-      <div>Elemento Destra</div>
-    </div>
-
-    <!-- Header Inferiore -->
-    <div class="header-inferiore d-flex justify-content-between align-items-center">
-      <div class="logo">
-        <img src="" alt="Logo 1" />
-      </div>
-      <nav class="navbar">
-        <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li><li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li><li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li><li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li><li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          
-        </ul>
-      </nav>
-      <div class="logo">
-        <img src="" alt="Logo 2" />
-      </div>
-    </div>
-  </header>
-</template>
-
-
 <style scoped>
+.purchase {
+  width: 120px;
+  height: 50px;
+  border-radius: 25px;
+  background-color: lightgreen;
+  text-align: center;
+  line-height: 50px;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
 .header-superiore {
   padding: 10px;
-  background-color: #f8f9fa;
+  background-color: grey;
   display: flex;
   justify-content: space-between;
 }
@@ -54,20 +91,28 @@ export default {
   background-color: #e9ecef;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 }
-  .logo img {
-    height: 50px;
-  }
 
-  .navbar .nav {
-    display: flex;
-    list-style: none;
-    padding: 0;
-  }
-    .nav-item .nav-link {
-      margin: 0 15px;
-      text-decoration: none;
-      color: #000;
-    }
+.logo img {
+  height: 50px;
+}
+
+.navbar .nav {
+  display: flex;
+  list-style: none;
+  padding: 0;
+}
+
+.nav-item .nav-link {
+  margin: 0 15px;
+  text-decoration: none;
+  color: #000;
+}
+
+.header-superiore nav ul li i {
+  font-size: 20px;
+  color: white;
   
+}
 </style>
